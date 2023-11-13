28ee20e4b6a8c46865071f422ac53a856d70685e
Md Azaz <mdazaz9026@gmail.com>
2023-11-13 18:48:00 +0530

    user , posts model

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,7 @@
 var express = require('express');
 var router = express.Router();
+const userModel = require('./users')
+const postModel = require('./posts')
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,34 @@
-var express = require('express');
-var router = express.Router();
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+const mongoose = require('mongoose');
+
+mongoose.connect('mongod://127.0.0.1:27017/pinterest')
+
+const userSchema = new mongoose.Schema({
+  username: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+
+  posts: [],
+
+  dp: {
+    type: String, // You might want to store the image URL or file path
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  fullname: {
+    type: String,
+    required: true,
+  },
 });
 
-module.exports = router;
+
+module.exports = mongoose.model('User', userSchema);
